836eaf9c24eabca272f5894ae1d05fee64a84112
James Page <jamesapage@btinternet.com>
2024-12-01 14:36:46 +0000

    Tidying up

--- days/1/day1.hs
+++ days/1/day1.hs
@@ -1,39 +1,33 @@
 module Main where
-import Data.List ( sort )
+import Data.List (sort)
 
 main :: IO ()
 main = do
     input <- readFile "days/1/input.txt"
-    print (part2 input)
+    let (left, right) = parse (words input) [] [] 
+    print (part2 left right)
 
+-- part1 :: String -> Int
+part1 :: (Num a, Ord a) => [a] -> [a] -> a
+part1 left right = 
+    sum (zipWith absDiff (sort left) (sort right))
 
-part1 :: String -> Int
-part1 input = 
-    let (left,right) = parse (words input) [] [] in
-        sum (zipWith absDiff (sort left) (sort right))
-
-
-part2 :: String -> Int
-part2 input = 
-    let (left,right) = parse (words input) [] [] in
-        productOccurences left right 0
-
+part2 :: [Int] -> [Int] -> Int
+part2 left right = 
+    productOccurences left right 0
 
 productOccurences :: [Int] -> [Int] -> Int -> Int
 productOccurences left right total =
     case left of
-        (a:xs) -> productOccurences xs right (total + a * countOcc a right)
+        (a:xs) -> productOccurences xs right (total + a * countOccurrences a right)
         [] -> total
 
-
-countOcc :: Int -> [Int] -> Int
-countOcc x list = 
+countOccurrences :: (Ord a) => a -> [a] -> Int
+countOccurrences x list = 
     length (filter (==x) list)
-
 
 absDiff :: Num c => c -> c -> c
 absDiff n = abs . (-) n
-
 
 parse :: [String] -> [Int] -> [Int] -> ([Int], [Int])
 parse text left right =
