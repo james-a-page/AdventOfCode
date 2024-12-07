294213a52fa4f2e36e45cc3288c68c4bd24dc3f1
James Page <jamesapage@btinternet.com>
2024-12-07 16:50:07 +0000

    slight day 7 performance for part 2?

--- days/7/day7.hs
+++ days/7/day7.hs
@@ -22,7 +22,11 @@ valid :: [Int -> Int -> Int] -> [Int] ->  Bool
 valid fs (target:components) = searchPossible fs target 0 components
     where searchPossible fs target total [] = total == target
           searchPossible fs target 0 (x:xs) = searchPossible fs target x xs
-          searchPossible fs target total (x:xs) = any (\f -> searchPossible fs target (f total x) xs) fs
+          searchPossible fs target total (x:xs) = any (\f -> let newTotal = f total x in (newTotal <= target) && searchPossible fs target newTotal xs) fs
 
-concatInt :: (Show a1, Show a2) => a1 -> a2 -> Int
-concatInt a b = read $ show a ++ show b :: Int
+-- concatInt :: (Show a1, Show a2) => a1 -> a2 -> Int
+concatInt :: Integral a => a -> a -> a
+concatInt x y = x * 10 ^ numDigits y + y
+  where
+    numDigits 0 = 1
+    numDigits n = floor (logBase 10 (fromIntegral n)) + 1
